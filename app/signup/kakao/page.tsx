8a9a1a971da2c97c39554a42702775745d32ee16
aaahyunseo <hs030608@naver.com>
2025-12-31 13:35:37 +0900

    hotfix: base url 코드 수정

--- app/signup/kakao/page.tsx
+++ app/signup/kakao/page.tsx
@@ -35,7 +35,7 @@ export default function KakaoSignupPage() {
       }
 
       const res = await fetch(
-        "http://localhost:8080/api/users/me/onboarding",
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/users/me/onboarding`,
         {
           method: "PATCH",
           headers: {
